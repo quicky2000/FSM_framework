2b610e09dc5d9219a0dbf910a157f7cef7a79e1a
Julien Thevenon <julien_thevenon@yahoo.fr>
2013-10-10 21:50:23 +0200

    Add algorithm to follow graph in deep

---
+++ include/algorithm_deep.h
@@ -0,0 +1,23 @@
+#ifndef ALGORITHM_DEEP
+#define ALGORITHM_DEEP
+
+#include "framework_algorithm_if.h"
+#include "situation_tree_node.h"
+#include "situation_manager.h"
+
+class algorithm_deep: public framework_algorithm_if
+{
+ public:
+  // Methods inherited from interface
+  void run(void);
+  std::string getString(void);
+  
+  static void registerAlgorithm(std::map<std::string,FSM_framework_algorithm_creator > &p_factory);
+ private:
+  std::map<const FSM_situation_if*,situation_tree_node> m_situation_tree;
+  situation_manager m_situation_manager;
+};
+
+framework_algorithm_if* createAlgorithmDeep(void);
+
+#endif /*ALGORITHM_DEEP */
---
+++ src/algorithm_deep.cpp
@@ -0,0 +1,154 @@
+#include "algorithm_deep.h"
+#include "FSM_if.h"
+#include "FSM_UI_if.h"
+#include "FSM_situation_if.h"
+#include "FSM_context_if.h"
+#include "FSM_transition_if.h"
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+//-----------------------------------------------------------------------------
+void algorithm_deep::run(void)
+{
+  if(getFsm() != NULL)
+    {
+      getFsm()->configure();
+      FSM_situation_if *l_previous_situation = NULL;
+      unsigned int l_previous_transition = 0;
+
+      bool l_continu = true;
+      do
+	{
+	  FSM_situation_if *l_current_situation = getFsm()->getCurrentSituation();
+	  std::cout << "Current situation :" << std::endl ;
+          getFsmUi()->displaySituation(l_current_situation) ;
+	  std::cout << "-----" << std::endl ;
+	      
+	  // Check if situation was already encoutered
+	  FSM_situation_if *l_unique_situation = m_situation_manager.getUniqueSituation(l_current_situation);
+	  map<const FSM_situation_if *,situation_tree_node>::iterator l_node_iter = m_situation_tree.find(l_unique_situation);
+	  if(l_node_iter == m_situation_tree.end())
+	    {
+	      std::cout << "New situation ! (never encountered)" << std::endl ;
+              cout << "Current situation : \"" << l_current_situation->getStringId() << "\"" <<endl ; 
+
+	      if(l_current_situation->isValid() && !l_current_situation->isFinal())
+		{
+		  cout << "Computing transitions" << endl ;
+		  getFsm()->computeTransitions();
+		}
+	      
+	      // Store current situation with its predecessor relation if it exists
+	      if(l_previous_situation != NULL)
+		{
+		  l_node_iter = m_situation_tree.insert(map<FSM_situation_if *,situation_tree_node>::value_type(l_unique_situation,situation_tree_node(l_current_situation,l_previous_situation,l_previous_transition))).first;
+		}
+	      else
+		{
+		  l_node_iter = m_situation_tree.insert(map<FSM_situation_if *,situation_tree_node>::value_type(l_unique_situation,situation_tree_node(l_current_situation))).first;
+		}
+	      cout << "Total of situation = " <<  m_situation_tree.size() << endl;
+	    }
+	  else if(l_current_situation != l_unique_situation)
+	    {
+	      l_current_situation = l_unique_situation; 
+	      getFsm()->setCurrentSituation(l_unique_situation);
+	    }
+	  
+	  situation_tree_node & l_situation_tree_node = l_node_iter->second;
+	  const set<unsigned int> &l_unexplored_transitions = l_situation_tree_node.getUnexploredTransitions();
+			
+          cout << "Situation valid = " << l_current_situation->isValid() << endl ;
+          cout << "Non explored transitions : " << l_unexplored_transitions.size() <<endl ;
+	  set<unsigned int>::const_iterator l_iter = l_unexplored_transitions.begin();
+	  set<unsigned int>::const_iterator l_iter_end = l_unexplored_transitions.end();
+	  while(l_iter != l_iter_end)
+	    {
+	      cout << *l_iter << " " ;
+	      l_iter++;
+	    }
+	  cout << endl ;
+ 
+	  // Check if it can go deeply
+	  if(l_unexplored_transitions.size() > 0)
+	    {
+	      // Store current situation to record the relationship of future situation
+	      l_previous_transition = *(l_unexplored_transitions.begin());
+	      l_previous_situation = l_current_situation;
+
+	      cout << "Select transition number : " << l_previous_transition << endl ;
+
+	      // Compute the next situation
+	      getFsm()->selectTransition(l_previous_transition);
+
+	      // Indicate that transition is explored
+	      l_situation_tree_node.setTransitionExplored(l_previous_transition);
+	    }
+	  // No more transition available so we go back for one step if possible
+	  else
+	    {
+	      
+	      const map<unsigned int,FSM_situation_if *> &l_predecessor_situations = l_situation_tree_node.getPredecessorSituations();
+	      // Check if there is a predecessor
+	      if(l_predecessor_situations.size())
+		{
+		  l_previous_situation = NULL;
+
+		  // Getting predecessor situation
+		  map<const FSM_situation_if *,situation_tree_node>::const_iterator l_previous_node_iter = 
+		    m_situation_tree.find(l_predecessor_situations.begin()->second);
+
+		  // Predecessor situation should be in situation tree !!!!
+		  assert(l_previous_node_iter != m_situation_tree.end());
+
+		  cout << "Restore previous situation " << l_previous_node_iter->second.getSituation() <<  endl ;
+		  // Getting back to previous situation
+		  getFsm()->setCurrentSituation(l_previous_node_iter->second.getSituation());
+		}
+	      else
+		{		  
+		  // We are at the original situation
+		  l_continu = false ;
+		}
+	    }
+	} while(l_continu);
+      cout << "End of algorithm" << endl ;
+    }
+  else
+    {
+      cout << "FSM ERROR : no FSM in algorithm" << endl ;
+    }
+}
+
+//-----------------------------------------------------------------------------
+string algorithm_deep::getString(void)
+{
+  return "algorithm_deep";
+}
+
+//-----------------------------------------------------------------------------
+void algorithm_deep::registerAlgorithm(map<string,FSM_framework_algorithm_creator> &p_factory)
+{
+  registerFrameworkAlgorithm("algorithm_deep",createAlgorithmDeep,p_factory);
+}
+
+//-----------------------------------------------------------------------------
+framework_algorithm_if* createAlgorithmDeep(void)
+{
+  return new algorithm_deep();
+}
+
+#ifdef EXTERNAL_ALGORITHM
+//-----------------------------------------------------------------------------
+extern "C"
+{
+  void registerAlgorithm(map<string,FSM_framework_algorithm_creator> &p_factory)
+  {
+    registerFrameworkAlgorithm("algorithm_deep",createAlgorithmDeep,p_factory);
+  }
+}
+#endif
+
+//EOF
