3672c3bdde376a57f1abfeebd69a72a2c48e5baf
Julien Thevenon <julien_thevenon@yahoo.fr>
2014-08-25 17:29:32 +0200

    Inline operator()

--- include/pointer_comparator.h
+++ include/pointer_comparator.h
@@ -18,23 +18,27 @@
 #ifndef POINTER_COMPARATOR_H
 #define POINTER_COMPARATOR_H
 
-namespace FSM_interfaces
-{
-  class FSM_situation_if;
-}
-
+#include "FSM_situation_if.h"
 namespace FSM_framework
 {
   class pointer_comparator// :public std::binary_function<T*,T*,bool>
   {
   
   public:
-    bool operator()(const FSM_interfaces::FSM_situation_if *p_op1,
-                    const FSM_interfaces::FSM_situation_if *p_op2)const;
+    inline bool operator()(const FSM_interfaces::FSM_situation_if *p_op1,
+                           const FSM_interfaces::FSM_situation_if *p_op2)const;
 
   private:
   
   };
+
+  //------------------------------------------------------------------------------
+  bool pointer_comparator::operator()(const FSM_interfaces::FSM_situation_if * p_op1,const FSM_interfaces::FSM_situation_if * p_op2)const
+  {
+    //assert(p_op1 && p_op2);
+    return p_op1->less(p_op2);
+  }
+
 }
 #endif // POINTER_COMPARATOR_H 
 //EOF
